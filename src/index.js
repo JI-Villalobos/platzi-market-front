b9af409f851e9fdb5306a6c4ffbb3d0a27fbeb09
arche <chuy12villalobos@gmail.com>
2021-07-25 11:33:40 -0500

    [UPDATE] login view

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import Login from './views/Login'
 
 
-const mainContent = document.querySelector("main")
-mainContent.innerHTML = `<login-form>`
+const mainContent = document.querySelector("body")
+mainContent.innerHTML = `
+    <main>
+        <div class="img-icon">
+            <img src="../src/assets/imgs/logo.svg" alt="">
+        </div>
+        <login-form>
+    </main>
+    `
